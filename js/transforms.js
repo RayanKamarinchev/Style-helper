6bd2abbd93f0bcc506f15318eada7761ccf37f3f
RayanKamarinchev <rayan2008bg@gmail.com>
2022-02-06 18:15:23 +0200

    transform chnager

---
+++ js/transforms.js
@@ -0,0 +1,20 @@
+const options = {
+    skew: "10deg, 30deg",
+    scale: "2, 4",
+    rotate: "90deg",
+    translate: "50px, 100px"
+};
+function OnSelect() {
+    const selector = document.getElementById("selection");
+    const textElement = document.getElementsByClassName("transform-example")[0];
+    console.log({selector:selector.value});
+    console.log({textElement});
+    textElement.innerText = options[selector.value];
+}
+
+function OnInputChange() {
+    const selector = document.getElementById("selection");
+    const inputElement = document.getElementById("input-text");
+    const image = document.getElementById("transform-example");
+    image.style.transform = `${selector.value}(${inputElement.value})`
+}
